077da79233c8d535b279f6701ad30f8e6b3eb4ac
rishiraj007 <rishiraj@mounttalent.com>
2022-12-30 17:39:27 +0530

    [Prabhu] Add: UnOrdered List

---
+++ DataStructureProgram/List.cs
@@ -0,0 +1,93 @@
+﻿using DataStructureProgram.UnOrderedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureProgram
+{
+    internal class List<T> where T : IComparable
+    {
+        public Node<T> head;
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into list", node.data);
+        }
+
+        public void Delete(T data)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+            }
+            if (head.data.Equals(data))
+            {
+                head = head.next;
+                return;
+            }
+            Node<T> prev = head;
+            Node<T> temp = head.next;
+            while (temp != null)
+            {
+                if (temp.data.Equals(data))
+                {
+                    prev.next = temp.next;
+                    temp = null;
+                    return;
+                }
+                prev = temp;
+                temp = temp.next;
+            }
+        }
+
+        public void DisplayList()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write("List elements are :" + temp.data + "\n");
+                temp = temp.next;
+            }
+        }
+
+        public void Search(T value)
+        {
+            Node<T> temp = this.head;
+            while (temp != null)
+            {
+                if (temp.data.Equals(value))
+                {
+                    Console.WriteLine("{0} is found in the list", temp.data);
+                    Delete(value);
+                    return;
+                }
+                temp = temp.next;
+            }
+            if (temp == null)
+            {
+                Console.WriteLine("{0} is not found in the list", value);
+                Add(value);
+            }
+        }
+    }
+}
--- DataStructureProgram/Program.cs
+++ DataStructureProgram/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
+            string filpath = @"D:\OneDrive\Documents\BridgeLabz\DataStructureProgram\DataStructureProgram\WordList.txt";
             Console.WriteLine("Welcome to Data Structure Program");
+            List<string> list = new List<string>();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect the operation to perform \n1.UnOrdered List \n2.Display List \n3.Search value \n4.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        string file = File.ReadAllText(filpath);
+                        string[] fileList = file.Split(",");
+                        foreach (var word in fileList)
+                        {
+                            list.Add(word);
+                        }
+                        break;
+                    case 2:
+                        list.DisplayList();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter value u want to search");
+                        string value = Console.ReadLine();
+                        list.Search(value);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
